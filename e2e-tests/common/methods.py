450e3055493a520a1d27c85676ffe15e6e592510
Julia Mendoza <231963277+jmendo10cosc381@users.noreply.github.com>
2025-12-10 20:26:03 +0000

    updated method.py

--- e2e-tests/common/methods.py
+++ e2e-tests/common/methods.py
@@ -48,3 +48,9 @@ def __get_multipart_form_data(file):
             "image": (file.name, file, "multipart/form-data"),
         }
     return multipart_form_data
+
+def genz_preview():
+    response = requests.get(
+        f"{ANONYMIZER_BASE_URL}/genz-preview", headers=DEFAULT_HEADERS
+    )
+    return response.status_code, response.content
